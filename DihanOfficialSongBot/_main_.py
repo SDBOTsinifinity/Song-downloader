44b3c69aaf0713aa07786bde3e867060bc0fc11f
Dihan Official <84884434+dihanofficial@users.noreply.github.com>
2021-06-14 07:29:53 +0530

    Update _main_.py

--- DihanOfficialSongBot/_main_.py
+++ DihanOfficialSongBot/_main_.py
@@ -1,1 +1,43 @@
+#Dihanofficial <https://t.me/dihan_official>
 
+from pyrogram.types.bots_and_keyboards import reply_keyboard_markup
+from JESongBot.plugins import *
+from pyrogram import idle, filters
+from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton
+from JESongBot import Jebot as app
+from JESongBot import LOGGER
+
+pm_start_text = """
+Hey [{}](tg://user?id={}), I'm Song Downloader Bot 🎵
+😉 Just send me the song name you want to download.😋
+      eg:```/song Despacito```
+      
+A bot by @dihanrandila 🇱🇰
+"""
+
+@app.on_message(filters.command("start"))
+async def start(client, message):
+    chat_id = message.chat.id
+    user_id = message.from_user["id"]
+    name = message.from_user["first_name"]
+    if message.chat.type == "private":
+        btn = InlineKeyboardMarkup(
+            [
+                [
+                     InlineKeyboardButton(
+                        text="Channel 🔊", url="https://t.me/dihanofficial"
+                    ),
+                    InlineKeyboardButton(
+                        text="Developer 🔥", url="https://t.me/dihanrandila"
+                    )
+                ]
+            ]
+        )
+    else:
+        btn = None
+    await message.reply(pm_start_text.format(name, user_id), reply_markup=btn)
+
+
+app.start()
+LOGGER.info("✅ DihanOfficialSongBot is online.")
+idle()
